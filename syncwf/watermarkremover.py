e44ffa908c6ab8c70e25f6c43d52710ada7b0b5d
meryaaam <mariemchouaiti@gmail.com>
2025-02-15 14:59:42 +0100

    refcator : Removed Arial font files and updated watermark remover script to use Amiri font and higher DPI for OCR accuracy.

--- syncwf/watermarkremover.py
+++ syncwf/watermarkremover.py
@@ -8,9 +8,9 @@ from arabic_reshaper import reshape  # For reshaping Arabic text
 
 def pdf_to_images(pdf_path):
     """
-    Convert PDF pages to images.
+    Convert PDF pages to images with a higher DPI for better OCR accuracy.
     """
-    images = convert_from_path(pdf_path)
+    images = convert_from_path(pdf_path, dpi=1300)  # Increase DPI to 300 or higher
     return images
 
 def ocr_images_to_text(images):
@@ -24,19 +24,21 @@ def ocr_images_to_text(images):
 
 def create_pdf_from_text(text, output_pdf):
     """
-    Create a new PDF from extracted text.
+    Create a new PDF from extracted text using a font that supports Arabic.
     """
     pdf = FPDF()
     pdf.add_page()
-    pdf.add_font('Arial', '', 'arial.ttf', uni=True)  # Use a font that supports Arabic
-    pdf.set_font('Arial', size=12) 
+    pdf.add_font('Amiri', '', 'font/armiri/Amiri-Regular.ttf', uni=True)  # Use a font that supports Arabic
+    pdf.set_font('Amiri', size=12)
+    pdf.set_right_margin(10)
     reshaped_text = reshape(text)  # Reshape Arabic text
     bidi_text = get_display(reshaped_text)  # Apply RTL direction
-    pdf.multi_cell(0, 10, bidi_text)  # Add text to PDF
+    #pdf.set_xy(190, 10)  # Adjust X position for right alignment
+
+    pdf.multi_cell(0, 10, bidi_text, align='R')  # Add text to PDF
     pdf.output(output_pdf)
 
-
-def remove_watermark(input_pdf, output_pdf, watermark_text=None, watermark_image_bbox=None):
+def remove_watermark(input_pdf, output_pdf):
     """
     Remove watermark by extracting text using OCR and recreating the PDF.
     """
@@ -51,16 +53,11 @@ def remove_watermark(input_pdf, output_pdf, watermark_text=None, watermark_image
     
     print(f"Watermark removed. Clean PDF saved to {output_pdf}")
 
-
-  
 def run():
-   remove_watermark(
-    input_pdf="input/54908.pdf",
-    output_pdf="output/clean_cassation/54908.pdf"
-   
-    # watermark_image_bbox=(x0, y0, x1, y1)  # Uncomment and specify coordinates for image
-)
-
+    remove_watermark(
+        input_pdf="input/54908.pdf",
+        output_pdf="output/clean_cassation/54908.pdf"
+    )
 
 if __name__ == "__main__":
     run()
